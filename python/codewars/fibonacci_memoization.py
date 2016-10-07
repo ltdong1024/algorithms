d340bfa373718b2427b0ac2bd20f98c72d75458c
Pablo Bordons Estrada <pablobordons@users.noreply.github.com>
2016-10-07 09:21:14 +0200

    Update fibonacci_memoization.py
    function declaration

--- python/codewars/fibonacci_memoization.py
+++ python/codewars/fibonacci_memoization.py
@@ -7,26 +7,26 @@
 # simple one (public cache)
 
 fibonacci_memo = {0: 0, 1: 1}
-def fibonacci(n):
+def fibonacci_public(n):
     if n in [0, 1]:
         return n
     if n not in fib_memo:
-        fib_memo[n] = fibonacci(n-1) + fibonacci(n-2)
+        fib_memo[n] = fibonacci_public(n-1) + fibonacci_public(n-2)
     return fib_memo[n-1] + fib_memo[n-2]
 
 
 # private cache
-def fibonacci(n, fib_memo=None):
+def fibonacci_private(n, fib_memo=None):
     if n in [0, 1]:
         return n
     if fib_memo is None:
     	fib_memo = { 0: 0, 1: 1}
     if n not in fib_memo:
-        fib_memo[n] = fibonacci(n-1, fib_memo) + fibonacci(n-2, fib_memo)
+        fib_memo[n] = fibonacci_private(n-1, fib_memo) + fibonacci_private(n-2, fib_memo)
     return fib_memo[n-1] + fib_memo[n-2]
     
 # private cache (succint)
-def fibonacci2(n, fib_memo = {0: 0, 1: 1}):
+def fibonacci(n, fib_memo = {0: 0, 1: 1}):
     if n not in prev:
         fib_memo[n] = fibonacci(n-1, fib_memo) + fibonacci(n-2, fib_memo)
     return fib_memo[n]
