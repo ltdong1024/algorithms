6be906e0cce8c97c06cbddb377a9db31a62c27cd
Pablo Bordons Estrada <pablobordons@users.noreply.github.com>
2016-10-07 09:09:09 +0200

    Create fibonacci_memoization.py
    Based on https://www.codewars.com/kata/memoized-fibonacci/train/python

---
+++ python/codewars/fibonacci_memoization.py
@@ -0,0 +1,33 @@
+'''
+  Memoization refers to remember results of previous method calls in order to avoid unnecessary repeated computations.
+  
+  Three cases: simple one (cache is public), private cache version (detailed) and succint private cache
+'''
+
+# simple one (public cache)
+
+fibonacci_memo = {0: 0, 1: 1}
+def fibonacci(n):
+    if n in [0, 1]:
+        return n
+    if n not in fib_memo:
+        fib_memo[n] = fibonacci(n-1) + fibonacci(n-2)
+    return fib_memo[n-1] + fib_memo[n-2]
+
+
+# private cache
+def fibonacci(n, fib_memo=None):
+    if n in [0, 1]:
+        return n
+    if fib_memo is None:
+    	fib_memo = { 0: 0, 1: 1}
+    if n not in fib_memo:
+        fib_memo[n] = fibonacci(n-1, fib_memo) + fibonacci(n-2, fib_memo)
+    return fib_memo[n-1] + fib_memo[n-2]
+    
+# private cache (succint)
+def fibonacci2(n, fib_memo = {0: 0, 1: 1}):
+    if n not in prev:
+        fib_memo[n] = fibonacci(n-1, fib_memo) + fibonacci(n-2, fib_memo)
+    return fib_memo[n]
+    
