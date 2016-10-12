53e8a47af76af7ec17ed1cfe1ecea8e1266e27c4
Pablo Bordons Estrada <pablobordons@users.noreply.github.com>
2016-10-12 20:50:57 +0200

    Create int_to_bin.py

---
+++ python/math/int_to_bin.py
@@ -0,0 +1,23 @@
+'''
+function to turn integers into binary numbers.
+It allows the user to especify the min length
+of the binary number, adding 0's if necessary.
+
+it requires math
+'''
+
+import math
+
+def int_to_bin(n, min_length=8):
+    max_power = int(math.ceil(math.log(n,2)))
+    powers = [pow(2,i) for i in range(max_power)][::-1]
+
+    binary = ""
+    for i in powers:
+        w = math.floor(n//i)
+        n = n - w*i
+        binary += str(int(w))
+    
+    binary = "0"*(min_length-len(binary)) + binary
+
+    return binary
