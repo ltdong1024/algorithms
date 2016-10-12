89e34a8c61d4b70e50916903c071993bfde5467f
Pablo Bordons Estrada <pablobordons@users.noreply.github.com>
2016-10-12 21:48:56 +0200

    Update int_to_bin.py
    not working for 0,1,2, hotfix added

--- python/math/int_to_bin.py
+++ python/math/int_to_bin.py
@@ -9,6 +9,13 @@ it requires math
 import math
 
 def int_to_bin(n, min_length=8):
+    if n == 0:
+        return "0"*min_length
+    if n == 1:
+        return "0"*(min_length - 1) + "1"
+    if n == 2:
+        return "0"*(min_length - 2) + "10"
+    
     max_power = int(math.ceil(math.log(n,2)))
     powers = [pow(2,i) for i in range(max_power)][::-1]
 
